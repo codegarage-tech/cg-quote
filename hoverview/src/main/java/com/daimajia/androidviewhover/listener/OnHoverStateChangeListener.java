36f1cb06dee0493579ded3427e59ce39c7c8e886
Md. Rashadul Alam <rashed.droid@gmail.com>
2017-11-14 21:07:16 +0600

    cg-quote: removed hoverview

--- hoverview/src/main/java/com/daimajia/androidviewhover/listener/OnHoverStateChangeListener.java
+++
@@ -1,9 +0,0 @@
-package com.daimajia.androidviewhover.listener;
-
-/**
- * Created by rashed on 11/13/17.
- */
-
-public interface OnHoverStateChangeListener {
-    public void onHoverStateChanged(boolean isHoverExist);
-}
