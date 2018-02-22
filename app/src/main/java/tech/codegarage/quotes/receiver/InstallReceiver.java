7343751dd024ab7d15438f50a34fa8aed32bef41
Md. Rashadul Alam <rashedul.alam@bjitgroup.com>
2018-02-22 14:55:48 +0600

    cg-quote: power connection and install receiver added

---
+++ app/src/main/java/tech/codegarage/quotes/receiver/InstallReceiver.java
@@ -0,0 +1,36 @@
+package tech.codegarage.quotes.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+/**
+ * @author Md. Rashadul Alam
+ *         Email: rashed.droid@gmail.com
+ */
+public class InstallReceiver extends BroadcastReceiver {
+
+    private static final String TAG = InstallReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent != null) {
+            String action = intent.getAction();
+            if (action != null) {
+                if (intent.getAction().equalsIgnoreCase(Intent.ACTION_PACKAGE_REMOVED)) {
+                    Log.d(TAG, "PACKAGE_REMOVED ");
+                } else if (intent.getAction().equalsIgnoreCase(Intent.ACTION_PACKAGE_ADDED)) {
+                    Log.d(TAG, "PACKAGE_ADDED ");
+                }
+
+                Uri data = intent.getData();
+                if (data != null) {
+                    String packageName = data.toString().split("\\:")[1];
+                    Log.d(TAG, "PackageName: " + packageName);
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/tech/codegarage/quotes/receiver/PowerConnectionReceiver.java
@@ -0,0 +1,30 @@
+package tech.codegarage.quotes.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * @author Md. Rashadul Alam
+ *         Email: rashed.droid@gmail.com
+ */
+public class PowerConnectionReceiver extends BroadcastReceiver {
+
+    private static final String TAG = PowerConnectionReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent != null) {
+            String action = intent.getAction();
+            if (action != null) {
+                if (action.equalsIgnoreCase(Intent.ACTION_POWER_CONNECTED)) {
+                    Log.d(TAG, "Power Connected");
+                } else if (action.equalsIgnoreCase(Intent.ACTION_POWER_DISCONNECTED)) {
+                    Log.d(TAG, "Power Disconnected");
+                }
+            }
+        }
+    }
+}
