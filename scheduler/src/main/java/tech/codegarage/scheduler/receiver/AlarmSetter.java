1677e6abf9aed1fd7a29b38a946d3e40a8c6e23a
Md. Rashadul Alam <rashedul.alam@bjitgroup.com>
2018-02-23 09:10:38 +0600

    cg-quote: quick boot mode added

--- scheduler/src/main/java/tech/codegarage/scheduler/receiver/AlarmSetter.java
+++ scheduler/src/main/java/tech/codegarage/scheduler/receiver/AlarmSetter.java
@@ -3,6 +3,9 @@ package tech.codegarage.scheduler.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.os.UserManagerCompat;
+import android.util.Log;
 
 import java.util.Calendar;
 
@@ -20,9 +23,33 @@ import static tech.codegarage.scheduler.util.AllConstants.SESSION_KEY_SCHEDULE_D
  */
 public class AlarmSetter extends BroadcastReceiver {
 
+    private static final String TAG = AlarmSetter.class.getSimpleName();
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        boolean bootCompleted = false;
+
+        //Check boot completed
+        if(intent !=null){
+            String action = intent.getAction();
+
+            if(action!=null){
+                Log.d(TAG, "Received action: " + action + ", user unlocked: " + UserManagerCompat.isUserUnlocked(context));
+
+                //Check boot type
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    bootCompleted = Intent.ACTION_LOCKED_BOOT_COMPLETED.equals(action);
+                } else {
+                    bootCompleted = Intent.ACTION_BOOT_COMPLETED.equals(action);
+                }
+            }
+        }
+        if (!bootCompleted) {
+            return;
+        }
+
+        //Set alarm
         if (!AppUtils.isNullOrEmpty(AppUtils.getStringSetting(context, SESSION_KEY_SCHEDULE_DATA, SESSION_DEFAULT_VALUE_STRING))) {
             ScheduleItem scheduleItem = ScheduleItem.convertFromStringToObject(AppUtils.getStringSetting(context, SESSION_KEY_SCHEDULE_DATA, SESSION_DEFAULT_VALUE_STRING), ScheduleItem.class);
 
